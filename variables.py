5e3f7b0e0ca84612040f703b3d72e3c5c19e4bdc
cesar <118922722+cesar1007141@users.noreply.github.com>
2023-08-27 02:06:18 -0500

    trabajo

---
+++ variables.py
@@ -0,0 +1,32 @@
+a = 1
+b = 2 
+y = 3
+
+x = a + b + y
+    
+print(x)
+
+
+"""
+NUMEROS ENTEROS
+Los números enteros son aquellos que no tienen decimales, 
+tanto positivos como negativos (además del cero). En Python 
+se pueden representar mediante el tipo int (de integer, entero) 
+o el tipo long (largo). La única diferencia es que el tipo long 
+permite almacenar números más grandes."""
+
+"""
+NUMEROS FLOTANTES
+Los números de punto flotante son números con decimales. 
+Por ejemplo, 1.5, 3.14 y -0.75 son números de punto flotante."""
+
+
+
+"""numeros pares en una variable."""
+
+q = 1
+l = 2
+
+n = q + l
+print(n)
+
